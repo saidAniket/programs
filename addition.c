d1faa55dc1d3dbfc27d0a2fab8b8f51ed2fa7560
saidAniket <119159622+saidAniket@users.noreply.github.com>
2022-11-27 15:08:47 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+int iNo=10;
+float fValue=3.14;
+double dData=6.10;
+char cValue=('m');
+
+printf("integer is %d \n",iNo);
+printf("float is %f \n",fValue);
+printf("double is %lf\n",dData);
+printf("character is %c \n",cValue);
+return 0;
+}
